e966a9a4fb07b1a66b0aa6b7aa5ead00cb9207f0
gavin rich <gavinrich@gmail.com>
2016-03-27 22:15:43 -0700

    added Ika's sprites to the blocks and her backdrop

---
+++ HwD/HwD/Assets/Scripts/FXTester.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FXTester : MonoBehaviour
+{
+    public GameObject particleExplosion = null;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+    void Explosion()
+    {
+        //create and destroy a particle 
+        GameObject explosion = GameObject.Instantiate(particleExplosion, gameObject.transform.position + new Vector3(0.0f, 0.0f, 12.0f), Quaternion.identity) as GameObject;
+        DestroyObject(explosion, 5.0f);
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+	
+	}
+}
